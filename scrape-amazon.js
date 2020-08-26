00cbd1dd3fd7bcd3fd4a734a956f26bee1d0cca7
alancphilpott <alanp1995@gmail.com>
2020-08-26 19:06:29 +0100

    app now enters a search term and conducts a search

--- scrape-amazon.js
+++ scrape-amazon.js
@@ -1,12 +1,12 @@
 const puppeteer = require("puppeteer");
 const cheerio = require("cheerio");
 
-const URL =
-    "https://www.amazon.co.uk/HP-Display-Full-1920-Monitor/dp/B07FW1M8SJ";
+const URL = "https://www.amazon.co.uk/";
 
 async function monitor() {
     let page = await browse();
-    await checkPrice(page);
+    await getAllProductsInSearch(page);
+    await getProductInfo(page);
 }
 
 async function browse() {
@@ -16,23 +16,45 @@ async function browse() {
     return page;
 }
 
-async function checkPrice(page) {
+async function getProductInfo(page) {
     await page.reload();
 
     let html = await page.evaluate(() => document.body.innerHTML);
     const $ = cheerio.load(html);
 
     const productTitle = $("#productTitle").text().trim();
-    console.log(productTitle)
+    console.log(productTitle);
 
     const originalPrice = $(".priceBlockStrikePriceString").text().trim();
-    console.log(originalPrice)
+    console.log(originalPrice);
 
     const discountPrice = $("#priceblock_ourprice").text();
     console.log(discountPrice);
 
-    const discountAmount = $(".priceBlockSavingsString").text().match(/\((.+?)\)/)[1];
+    const discountAmount = $(".priceBlockSavingsString")
+        .text()
+        .match(/\((.+?)\)/)[1];
     console.log(discountAmount);
+}
+
+async function getAllProductsInSearch(page) {
+    await page.reload();
+
+    let html = await page.evaluate(() => document.body.innerHTML);
+    const $ = cheerio.load(html);
+
+    try {
+        await page.click("input#sp-cc-accept");
+    } catch (err) {
+        console.log("Cookies Already Accepted.");
+    }
+
+    await page.$eval(
+        "#twotabsearchtextbox",
+        (box) => (box.value = "windows 10 laptop")
+    );
+
+    await (await page.$('input[type="text"].nav-input')).press("Enter");
 }
 
 monitor();
