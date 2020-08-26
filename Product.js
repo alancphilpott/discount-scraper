1f358b22ccc627c98ee895c9cfe534220dcd9509
alancphilpott <alanp1995@gmail.com>
2020-08-26 22:46:01 +0100

    create a product class to assign attributes from web page to

---
+++ Product.js
@@ -0,0 +1,8 @@
+class Product {
+    constructor(title, originalPrice, discountPrice, discountAmount) {
+        this.title = title;
+        this.originalPrice = originalPrice;
+        this.discountPrice = discountPrice;
+        this.discountAmount = discountAmount;
+    }
+}
